9761fd5b87ce07b01b3eae2ac1d6dddcab08fb7f
Daniil <mr.gutsy@bk.ru>
2021-08-30 12:57:44 +0300

    Initial commit

---
+++ src/main/java/ru/mrgutsy/spring_boot/dao/UserRepository.java
@@ -0,0 +1,13 @@
+package ru.mrgutsy.spring_boot.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import ru.mrgutsy.spring_boot.model.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    User findByUsername(String username);
+
+    User findByEmail(String email);
+}
---
+++ src/main/java/ru/mrgutsy/spring_boot/service/UserDetailsServiceImpl.java
@@ -0,0 +1,29 @@
+package ru.mrgutsy.spring_boot.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import ru.mrgutsy.spring_boot.model.User;
+
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private UserService userService;
+
+    @Autowired
+    public void setUserService(UserService userService) {
+        this.userService = userService;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userService.getUserByEmail(email);
+        if (user == null) {
+            throw new UsernameNotFoundException("Email not found " + email);
+        }
+        return user;
+    }
+}
