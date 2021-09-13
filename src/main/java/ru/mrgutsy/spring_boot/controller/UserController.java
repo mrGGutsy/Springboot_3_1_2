a32e00010b9b845f75e522964bb213f67d40ccfd
Daniil <mr.gutsy@bk.ru>
2021-09-13 10:32:48 +0300

    add AdminController

--- src/main/java/ru/mrgutsy/spring_boot/controller/UserController.java
+++ src/main/java/ru/mrgutsy/spring_boot/controller/UserController.java
@@ -5,13 +5,11 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-import ru.mrgutsy.spring_boot.model.Role;
 import ru.mrgutsy.spring_boot.model.User;
 import ru.mrgutsy.spring_boot.service.RoleService;
 import ru.mrgutsy.spring_boot.service.UserService;
 
-import java.util.HashSet;
-import java.util.Set;
+
 
 @Controller
 public class UserController {
@@ -37,46 +35,4 @@ public class UserController {
         return "userpage";
     }
 
-    @GetMapping(value = "/admin")
-    public String listUsers(@AuthenticationPrincipal User user, Model model) {
-        model.addAttribute("user", user);
-        model.addAttribute("allUsers", userService.getAllUsers());
-        model.addAttribute("allRoles", roleService.getAllRoles());
-        return "adminpage";
-    }
-
-    @GetMapping(value = "/admin/new")
-    public String newUser(Model model) {
-        model.addAttribute("user", new User());
-        model.addAttribute("roles", roleService.getAllRoles());
-        return "new";
-    }
-
-    @PostMapping(value = "/admin/add-user")
-    public String addUser(@ModelAttribute User user, @RequestParam(value = "checkBoxRoles") String[] checkBoxRoles) {
-        Set<Role> roleSet = new HashSet<>();
-        for (String role : checkBoxRoles) {
-            roleSet.add(roleService.getRoleByName(role));
-        }
-        user.setRoles(roleSet);
-        userService.addUser(user);
-        return "redirect:/admin";
-    }
-
-    @PostMapping(value = "/admin")
-    public String editUser(@ModelAttribute User user, @RequestParam(value = "checkBoxRoles") String[] checkBoxRoles) {
-        Set<Role> roleSet = new HashSet<>();
-        for (String roles : checkBoxRoles) {
-            roleSet.add(roleService.getRoleByName(roles));
-        }
-        user.setRoles(roleSet);
-        userService.updateUser(user);
-        return "redirect:/admin";
-    }
-
-    @PostMapping(value = "/admin/{id}/delete")
-    public String removeUser(@PathVariable("id") long id) {
-        userService.removeUserById(id);
-        return "redirect:/admin";
-    }
 }
